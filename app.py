fd5037f1cda95500f81dfcd15543fcd7b8cb3eba
Sushant <sushant3660jee@gmail.com>
2024-06-27 01:31:56 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template
+app= Flask(__name__)
+@app.route("/")
+def start():
+    return "Server is runing"
+
+@app.route("/alpha")
+def end():
+    return render_template('index.html')
+
+
