35bb3c225a2fb2acf5b1ee92c7cbe75dec901f9d
Sushant <sushant3660jee@gmail.com>
2024-06-27 01:56:12 +0530

    fin

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
-from flask import Flask,render_template,requests
+from flask import Flask,render_template
 app= Flask(__name__)
 
-def get_user_submissions(handle):
-    url = f"https://codeforces.com/api/user.status?handle={handle}"
-    response = requests.get(url)
-    if response.status_code != 200:
-        print(f"Failed to load user submissions for {handle}.")
-        return []
 
-    data = response.json()
-
-    if data["status"] != "OK":
-        print(f"Error: {data['comment']}")
-        return []
-
-    return data["result"]
 @app.route("/")
 def start():
-
     return "Server is runing"
 
 @app.route("/alpha")
 def end():
-    # data=get_user_submissions('Sushant81')
-    return 'hi'
+    return render_template('index.html')
 
 
